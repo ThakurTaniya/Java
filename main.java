c4e0e3e3beabc51c41352d7cf79f86e1e4b05e21
Taniya Thakur <thakurtanya633@gmail.com>
2026-09-22 12:25:13 +0530

    java

--- main.java
+++ main.java
@@ -1,9 +1,7 @@
 public class main {
-   
-
-   public static void main(String[] var0) {
-      System.out.println("Hello, World!");
-   }
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
 }
 
 
